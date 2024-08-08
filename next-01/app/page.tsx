4d13ab79f6aebc243f0c79748da0e8920a17ce95
WiBuMoney <cowsmile1911@gmail.com>
2024-08-08 21:58:54 +0700

    *Update: - Data Fetching, Dynamic Routes & Metadata

--- next-01/app/page.tsx
+++ next-01/app/page.tsx
@@ -4,28 +4,30 @@ export default function Home() {
 	return (
 		<main
 			className="
-        relative
-        min-h-screen
-      "
+				relative
+				min-h-screen
+			"
 		>
 			<h1
 				className="
-        text-3xl 
-        absolute
-        mx-auto
-        top-24 bottom-0 inset-x-0
-        text-center
-        h-max"
+					text-3xl 
+					absolute
+					mx-auto
+					top-24 bottom-0 inset-x-0
+					text-center
+					h-max
+				"
 			>
 				Hello World!
 			</h1>
 			<Link
 				href="/about"
 				className="
-          absolute bottom-40 left-0 right-0
-          text-center
-          mx-auto
-          italic underline text-teal-400"
+					absolute bottom-40 left-0 right-0
+					text-center
+					mx-auto
+					italic underline text-teal-400
+				"
 			>
 				Go to about page!
 			</Link>
---
+++ next-02/lib/getUser.tsx
@@ -0,0 +1,5 @@
+export default async function getUser(userId: string) {
+	const response = await fetch(`https://jsonplaceholder.typicode.com/users/${userId}`);
+	if (!response.ok) throw Error("Failed to fetch user!");
+	return response.json();
+}
